66b4e0f060139fd0a5ec9f0546fd90af185dd4aa
Bernard Lambeau <blambeau@gmail.com>
2011-07-19 17:05:43 +0200

    Enhanced decorate benchmark

--- bench/bench_decorate.rb
+++ bench/bench_decorate.rb
@@ -3,8 +3,8 @@ require 'yargi'
 Bench.runner do |b|
   b.variation_point :version, Yargi::VERSION
   b.variation_point :ruby, Bench.which_ruby
-  b.range_over([50,100,150,200,250,300,350,400,450,500], :size) do |size|
-    b.range_over(1..10, :i) do
+  b.range_over([100,200,300,400,500], :size) do |size|
+    b.range_over(1..5, :i) do
       bench_case = Yargi::Digraph.random(size, 4*size)
       b.report(:vertex_count => bench_case.vertex_count, 
                :test => :depth) do
--- bench/runall.sh
+++ bench/runall.sh
@@ -1,2 +1,2 @@
-bench -Ilib run bench/bench_decorate.rb > bench/bench_decorate.rash
+bench -Ilib run bench/bench_decorate.rb >> bench/bench_decorate.rash
 bench plot bench/bench_decorate.rash -x vertex_count -y real -g version -s ruby --gnuplot=gif | gnuplot > bench/bench_decorate.gif
